de88c1b5088da257feb773b67ec3ace96285e931
kuavo_CI <kuavo_CI@50_eb_f6_bd_54_c1>
2026-05-14 11:15:34 +0800

    sync https://www.lejuhub.com/highlydynamic/lejulab/commit/446beced72c6acdc6855485e97a4460beba4e412

--- docs/leju-sdk/_static/documentation_options.js
+++ docs/leju-sdk/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2b37ae9e',
+    VERSION: '436fe580a0',
     LANGUAGE: 'zh-CN',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
